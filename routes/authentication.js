12eabe7345b499a78676cad92984361d691cb4c6
bentitataher <bentitataher@gmail.com>
2021-10-09 17:42:09 +0100

    Tests Git ok

--- routes/authentication.js
+++ routes/authentication.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 
-// Inscription user - un commentaire de test pour git encore
+// Inscription user
 router.post('/signup', function (req, res, next) {
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
