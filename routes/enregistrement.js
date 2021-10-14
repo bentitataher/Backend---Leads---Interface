1b70f27543fcedb3a95dc6dee730330e10131ee6
bentitataher <bentitataher@gmail.com>
2021-10-14 02:29:30 +0000

    multer working from postman : OK

--- routes/enregistrement.js
+++ routes/enregistrement.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
+
 const Enregistrement = require('../models/enregistrement');
 
-router.post('/ajout', function (req, res, next) {
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'uploads');
+    },
+    filename: (res, file, cb) =>{
+        const newFileName = Date.now() + path.extname(file.originalname);
+        cb(null, newFileName)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter : fileFilter})
+
+router.post('/ajout', upload.single('image') , function (req, res, next) {
+    req.body.enregistrement = req.file.path
     Enregistrement.create(req.body)
                   .then((enregistrement) =>{
-                      res.status(203).send(req.body)
+                      res.status(209).send(enregistrement)
                   })
 });
 
